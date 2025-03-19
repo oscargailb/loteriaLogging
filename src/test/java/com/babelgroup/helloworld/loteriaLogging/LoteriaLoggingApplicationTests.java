49193cd24863b65f79c88912c084c60d398f952c
oscargailb <oscar.gail@babelgroup.com>
2025-03-19 10:14:06 +0100

    Primer commit

---
+++ src/test/java/com/babelgroup/helloworld/loteriaLogging/LoteriaLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babelgroup.helloworld.loteriaLogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteriaLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
